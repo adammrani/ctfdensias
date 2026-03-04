f598afd1d7152dd3c67e681b46c48b7681c04be8
adammrani <adammrani15@gmail.com>
2026-03-04 22:34:26 +0100

    Add files via upload

---
+++ ctfdensias/src/test/java/com/ctfdensias/CtfdensiasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ctfdensias;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CtfdensiasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
