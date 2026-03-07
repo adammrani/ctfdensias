2f34be0274f89d4441361d59346550f2bb98f357
adammrani <kulusevski@DESKTOP-3H29BKU.localdomain>
2026-03-07 00:37:26 +0000

    removed old folder, backend working

--- ctfdensias/src/main/java/com/ctfdensias/model/Difficulty.java
+++
@@ -1,5 +0,0 @@
-package main.java.com.ctfdensias.model;
-
-public enum Difficulty {
-    EASY, MEDIUM, HARD
-}
--- ctfdensias/src/main/java/com/ctfdensias/model/Role.java
+++
@@ -1,5 +0,0 @@
-package com.ctfdensias.model;
-
-public enum Role {
-    USER, ADMIN
-}
